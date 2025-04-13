b77fbd410484f386f0401cfdbb9faa6f40131e2c
Yasin Efem  Dalkılıç <39165806+yasinfmd@users.noreply.github.com>
2025-04-13 20:28:41 +0300

    type declartion support added

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'OwllDebugWidget',
-      fileName: (format) => `owll-debug-widget.${format}.js`
+      fileName: (format) => `owll-debug-widget.${format}.js`,
     },
     rollupOptions: {
       output: {
-        // UMD build için global değişken adı
         globals: {
-          'owll-debug-widget': 'OwllDebugWidget'
-        }
-      }
-    }
-  }
+          'owll-debug-widget': 'OwllDebugWidget',
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true, 
+      outputDir: resolve(__dirname, 'dist/types'), 
+    }),
+  ],
 });
